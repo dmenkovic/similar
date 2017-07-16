1745f19be3055991aa2188b8b97230174745d140
dmenkovic <menkovicd.83@gmail.com>
2017-07-16 16:52:05 +0200

    Create index.php
    Associative array, 
    JavaScript Object -only one object and Object Constructor -many objects of one type.

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<!-- Associative array -->
+<?php
+$person = array("firstName"=>"Dobrica", "lastName"=>"Menkovic", "age"=>"34");
+echo $person['firstName'] . " is " . $person['age'] . " years old. ----- ( PHP Assoc.Array )";
+?>
+
+<p id="demo1"></p>
+<p id="demo2"></p>
+
+<!-- JS Objects -->
+<script>
+var person = { firstName:"Dobrica", lasttName:"Menkovic", age:34 };
+document.getElementById("demo1").innerHTML = person.firstName + " is " + person.age + " years old. ----- ( JS Object )";
+
+// Object Constructor:
+function Person(first, last, age) {
+    this.fName = first;
+    this.lName = last;
+    this.age = age;
+}
+var myFather = new Person ("Stanimir", "Menkovic", 70);
+var myMother = new Person ("Nadica", "Menkovic", 55);
+document.getElementById("demo2").innerHTML = myMother.fName + " is " + myMother.age + " years old. ----- ( JS Object Constructor )";
+</script>
+</body>
+</html>
